2f568a831b4f6cd1be7788fe29c7f5c970c1106d
Arun Rathor <arun.rathor124@gmail.com>
2026-09-25 15:58:01 +0530

    java basic

---
+++ baiscs java/java01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class java01 {
+    public static void main(String[] args) {
+
+        System.out.println("Enter your employee id : ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        System.out.println("hello employee id : " + a);
+    }
+}
